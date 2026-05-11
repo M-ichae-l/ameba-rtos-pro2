895cad8def31f38e02a2fbe56b37e023fdf694bd
tiewyenhuei <TiewYenHuei@realtek-sg.com>
2026-05-11 10:02:52 +0800

    [amebapro2][video] add SC450AI and OV5647 sensor drivers
    * add SC450AI and OV5647 sensor drivers
    
    <PATCH>
    title:add SC450AI and OV5647 sensor drivers
    </PATCH>
    
    <README>
    1. add SC450AI and OV5647 sensor drivers.
    2. update sensor.h to include JX-F57, SC450AI and OV5647.
    
    <FILELIST>
    </README>
    
    Change-Id: Id2337d30291cf38a9e72a715bf54a7b9738014cc

--- project/realtek_amebapro2_v0_example/inc/sensor.h
+++ project/realtek_amebapro2_v0_example/inc/sensor.h
@@ -90,6 +90,9 @@ struct sensor_params_t {
 #define SENSOR_OV12890         0x43  //  |   v    |   v    |   -    |   -    |
 #define SENSOR_OV12890_12M     0x44  //  |   v    |   -    |   -    |   -    |
 #define SENSOR_OV12890_12M_SEQ 0x45  //  |   v    |   -    |   -    |   -    |
+#define SENSOR_F57				0x46  //  |   v    |   -    |   -    |   -    |
+#define SENSOR_SC450AI			0x47  //  |   v    |   -    |   -    |   -    |
+#define SENSOR_OV5647		  	0x48  //  |   v    |   -    |   -    |   -    |
 
 static const struct sensor_params_t sensor_params[] = {
 	[SENSOR_DUMMY]        = {1920, 1080, 30},
@@ -162,6 +165,9 @@ static const struct sensor_params_t sensor_params[] = {
 	[SENSOR_OV12890]  	      = {2048,  1536, 30},
 	[SENSOR_OV12890_12M]      = {4032,  3024, 8},
 	[SENSOR_OV12890_12M_SEQ]  = {2112,  1616, 5}, //width = 2016 + 96(overlap) //height = 1512 + 104(overlap)
+	[SENSOR_F57]			  = {1920, 1080, 30},
+	[SENSOR_SC450AI]		  = {2560, 1440, 30},
+	[SENSOR_OV5647]			  = {2592, 1944, 15},
 };
 
 #define SENSOR_MAX         5
